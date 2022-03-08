d1592c6b2d985dbbada88b0855facac6f8fff8bb
united cripto <unitedcripto@gmail.com>
2022-03-07 18:49:14 -0600

    initial

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<div class="index">
+  <h2>Welcome</h2>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda laborum
+    aliquid labore incidunt at repudiandae totam quibusdam quis, architecto
+    illum quos asperiores fugit inventore iusto odio itaque maxime. Repellendus,
+    temporibus.
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/about">About</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
